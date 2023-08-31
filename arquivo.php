8948c2e7f564113f977e97f88cdc1a954c371c63
andrebalexandre <andre_alexandre@estudante.sesisenai.org.br>
2023-08-31 08:29:47 -0300

    andré

---
+++ arquivo.php
@@ -0,0 +1,32 @@
+<?php
+
+$matricula = $_POST["matricula"];    // Campos "name" inseridos no <form>
+$nome = $_POST["nome"];
+
+include('conecta.php');
+$comando = $pdo->prepare("INSERT INTO alunos( Matricula, Nome) Values( $matricula ,'$nome')");
+$resultado = $comando->execute();
+
+$empregados = [];  // Variável usada para guardar os dados acima no formato JSON.
+
+
+
+// O trecho abaixo pode estar dentro de um WHILE para guardar a resposta de um SELECT por exemplo
+// Neste exemplo estamos apenas devolvendo o que o usuário digitou no formulário
+$resposta = 
+      [
+         "matricula"  => $matricula,
+         "nome" => $nome
+         
+      ];
+
+array_push($empregados, $resposta);
+// Até aqui ficaria dentro do WHILE.
+
+
+
+// Ao sair do WHILE enviamos de volta para a função JavaScript no formato JSON:
+$json_texto = json_encode(["empregados" => $empregados]);
+echo($json_texto);  // Será retornado para dentro do "success" do arquivo index.html
+
+?>
