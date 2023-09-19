cd2ea2359165f62c0879d9ae373fa62238facf5a
andrebalexandre <andre_alexandre@estudante.sesisenai.org.br>
2023-09-19 12:26:28 -0300

    alterandré

--- arquivo.php
+++ arquivo.php
@@ -1,40 +1,45 @@
 <?php
 
-$data = $_POST["data"];    // Campos "name" inseridos no <form>
+$data = $_POST["data"];
 $sexo = $_POST["sexo"];
 $nome_hospital = $_POST["nome_hospital"];
 $nomepac = $_POST["NOMEPAC"];
 $idadepac = $_POST["IDADEPAC"];
 $cpfpac = $_POST["CPFpac"];
-$telefone = $_POST["Telefone"];
+$telefone = $_POST["telefone"];
 
 include('conecta.php');
-$comando = $pdo->prepare("INSERT INTO paciente( Data , Sexo, Nome_hospital, Nomepac, Idadepac, CPFpac, Telefone) Values( $data, $sexo, '$nome_hospital','$nomepac',$idadepac, $cpfpac, $telefone)");
+
+// Use prepared statements para evitar injeção de SQL
+$comando = $pdo->prepare("INSERT INTO paciente(Data, Sexo, Nome_hospital, Nomepac, Idadepac, CPFpac, Telefone) VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+// Associe os valores aos parâmetros da consulta
+$comando->bindParam(1, $data);
+$comando->bindParam(2, $sexo);
+$comando->bindParam(3, $nome_hospital);
+$comando->bindParam(4, $nomepac);
+$comando->bindParam(5, $idadepac);
+$comando->bindParam(6, $cpfpac);
+$comando->bindParam(7, $telefone);
+
 $resultado = $comando->execute();
 
-$empregados = [];  // Variável usada para guardar os dados acima no formato JSON.
+if ($resultado) {
+    $empregados = [
+        [
+            "aniversario" => $data,
+            "sexo" => $sexo,
+            "nome_hospital" => $nome_hospital,
+            "NOMEPAC" => $nomepac,
+            "IDADEPAC" => $idadepac,
+            "CPFpac" => $cpfpac,
+            "Telefone" => $telefone
+        ]
+    ];
 
+} else {
+    // Tratamento de erro, se necessário
+}
 
-// O trecho abaixo pode estar dentro de um WHILE para guardar a resposta de um SELECT por exemplo
-// Neste exemplo estamos apenas devolvendo o que o usuário digitou no formulário
-$resposta = 
-      [
-         "aniversario"  => $data,
-         "sexo" => $sexo
-         "nome_hospital" => $nome_hospital
-         "NOMEPAC" => $nomepac
-         "IDADEPAC" => $idadepac
-         "CPFpac" => $cpfpac
-         "Telefone" => $telefone
-      ];
-
-array_push($empregados, $resposta);
-// Até aqui ficaria dentro do WHILE.
-
-
-
-// Ao sair do WHILE enviamos de volta para a função JavaScript no formato JSON:
-$json_texto = json_encode(["empregados" => $empregados]);
-echo($json_texto);  // Será retornado para dentro do "success" do arquivo index.html
 
 ?>
